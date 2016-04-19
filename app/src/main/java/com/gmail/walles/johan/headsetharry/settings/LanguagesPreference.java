89db91793f64ce2b8958ad88713450121f8ada03
Johan Walles <johan.walles@gmail.com>
2016-04-19 15:22:49 +0200

    Move click handling where it should be

--- app/src/main/java/com/gmail/walles/johan/headsetharry/settings/LanguagesPreference.java
+++ app/src/main/java/com/gmail/walles/johan/headsetharry/settings/LanguagesPreference.java
@@ -11,22 +11,19 @@ public class LanguagesPreference extends Preference {
         super(context, attributes);
 
         setSummary("Swedish, English");
+    }
 
-        setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-            @Override
-            public boolean onPreferenceClick(Preference preference) {
-                new AlertDialog.Builder(context).
-                    setMessage("You should have been taken to a pick-languages activity").
-                    setNeutralButton(android.R.string.ok, new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            dialog.dismiss();
-                        }
-                    }).
-                    create().
-                    show();
-                return true;
-            }
-        });
+    @Override
+    protected void onClick() {
+        new AlertDialog.Builder(getContext()).
+            setMessage("You should have been taken to a pick-languages activity").
+            setNeutralButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            }).
+            create().
+            show();
     }
 }
--- app/src/main/java/com/gmail/walles/johan/headsetharry/settings/SettingsFragment.java
+++ app/src/main/java/com/gmail/walles/johan/headsetharry/settings/SettingsFragment.java
@@ -5,12 +5,7 @@ import android.preference.PreferenceFragment;
 
 import com.gmail.walles.johan.headsetharry.R;
 
-import org.jetbrains.annotations.NonNls;
-
 public class SettingsFragment extends PreferenceFragment {
-    @NonNls
-    public static final String ACTIVE_LANGUAGES_LIST = "activeLanguagesList";
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
