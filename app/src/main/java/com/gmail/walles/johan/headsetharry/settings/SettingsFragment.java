2cb147a43d500b23ab1f8c75110e514976950953
Johan Walles <johan.walles@gmail.com>
2016-04-19 14:59:59 +0200

    Show a custom preference that is clickable

--- app/src/main/java/com/gmail/walles/johan/headsetharry/settings/SettingsFragment.java
+++ app/src/main/java/com/gmail/walles/johan/headsetharry/settings/SettingsFragment.java
@@ -1,6 +1,41 @@
 package com.gmail.walles.johan.headsetharry.settings;
 
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.support.v7.app.AlertDialog;
+
+import com.gmail.walles.johan.headsetharry.R;
+
+import org.jetbrains.annotations.NonNls;
 
 public class SettingsFragment extends PreferenceFragment {
+    @NonNls
+    public static final String ACTIVE_LANGUAGES_LIST = "activeLanguagesList";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.preferences);
+
+        Preference activeLanguagesPref = findPreference(ACTIVE_LANGUAGES_LIST);
+        activeLanguagesPref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                new AlertDialog.Builder(getActivity()).
+                    setMessage("You should have been taken to a pick-languages activity").
+                    setNeutralButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    }).
+                    create().
+                    show();
+                return true;
+            }
+        });
+    }
 }
