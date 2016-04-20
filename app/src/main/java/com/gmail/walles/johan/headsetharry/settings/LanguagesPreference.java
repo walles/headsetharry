1bfadb6ece5311aa42bb36661d7b62e2c7ec5385
Johan Walles <johan.walles@gmail.com>
2016-04-20 13:44:48 +0200

    Use MultiSelectListPreference
    It was there, no need to re-implement it.

--- app/src/main/java/com/gmail/walles/johan/headsetharry/settings/LanguagesPreference.java
+++ app/src/main/java/com/gmail/walles/johan/headsetharry/settings/LanguagesPreference.java
@@ -1,21 +1,72 @@
 package com.gmail.walles.johan.headsetharry.settings;
 
 import android.content.Context;
-import android.preference.Preference;
+import android.preference.MultiSelectListPreference;
 import android.util.AttributeSet;
 
-public class LanguagesPreference extends Preference {
+import com.optimaize.langdetect.i18n.LdLocale;
+import com.optimaize.langdetect.profiles.BuiltInLanguages;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Locale;
+
+public class LanguagesPreference extends MultiSelectListPreference {
     public LanguagesPreference(final Context context, AttributeSet attributes) {
         super(context, attributes);
 
-        // FIXME: Subscribe to changes in our preference
+        populateList();
 
-        // FIXME: Set summary from the current preference value
-        setSummary("Swedish, English");
+        // FIXME: Do this based on current settings, and keep up to date
+        setSummary("English, Swedish");
     }
 
-    @Override
-    protected void onClick() {
-        LanguagesPickerActivity.start(getContext());
+    private void populateList() {
+        // List the languages supported by our language detector
+        List<LdLocale> languages = BuiltInLanguages.getLanguages();
+        LdLocale available[] = languages.toArray(new LdLocale[languages.size()]);
+
+        Arrays.sort(available, new Comparator<LdLocale>() {
+            @Override
+            public int compare(LdLocale lhs, LdLocale rhs) {
+                String l0 = new Locale(lhs.getLanguage()).getDisplayName();
+                String l1 = new Locale(rhs.getLanguage()).getDisplayName();
+                return l0.compareTo(l1);
+            }
+        });
+
+        CharSequence entries[] = new CharSequence[available.length];
+        CharSequence values[] = new CharSequence[available.length];
+        for (int i = 0; i < available.length; i++) {
+            LdLocale current = available[i];
+            entries[i] = parseLocale(current.toString()).getDisplayName();
+            values[i] = current.toString();
+        }
+
+        setEntries(entries);
+        setEntryValues(values);
+    }
+
+    private Locale parseLocale(String string) {
+        String parts[] = string.split("_", -1);
+        if (parts.length == 1) {
+            parts = string.split("-", -1);
+        }
+
+        if (parts.length == 1) {
+            return new Locale(parts[0]);
+        }
+
+        if (parts.length == 2
+            || (parts.length == 3 && parts[2].startsWith("#"))) {
+            return new Locale(parts[0], parts[1]);
+        }
+
+        if (parts.length == 3) {
+            return new Locale(parts[0], parts[1], parts[2]);
+        }
+
+        throw new IllegalArgumentException("Failed to parse locale string: <" + string + ">");
     }
 }
