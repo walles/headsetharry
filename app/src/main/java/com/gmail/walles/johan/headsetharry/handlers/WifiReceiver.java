70a16abcf023ea72c04a107f8d83c1bd29557e07
Johan Walles <johan.walles@gmail.com>
2016-04-25 12:07:32 +0200

    Announce WiFi events only once
    Got some code from StackOverflow, where else:
    http://stackoverflow.com/a/5890104/473672

--- app/src/main/java/com/gmail/walles/johan/headsetharry/handlers/WifiReceiver.java
+++ app/src/main/java/com/gmail/walles/johan/headsetharry/handlers/WifiReceiver.java
@@ -9,6 +9,8 @@ import android.net.wifi.WifiManager;
 
 import com.gmail.walles.johan.headsetharry.LoggingUtil;
 
+import java.util.Locale;
+
 import timber.log.Timber;
 
 public class WifiReceiver extends BroadcastReceiver {
@@ -16,23 +18,38 @@ public class WifiReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         LoggingUtil.setUpLogging(context);
 
-        if (!WifiManager.NETWORK_STATE_CHANGED_ACTION.equals(intent.getAction())) {
-            Timber.w("WifiReceiver got unexpected action <%s>", intent.getAction());
+        NetworkInfo networkInfo =
+            intent.getParcelableExtra(WifiManager.EXTRA_NETWORK_INFO);
+        if (networkInfo == null) {
+            Timber.w("Got null NetworkInfo from action <%s>", intent.getAction());
             return;
         }
 
-        NetworkInfo networkInfo = intent.getParcelableExtra(WifiManager.EXTRA_NETWORK_INFO);
-        if (ConnectivityManager.TYPE_WIFI != networkInfo.getType()) {
-            Timber.d("WifiReceiver got non-Wifi event %d", networkInfo.getType());
-            return;
+        @SuppressWarnings("HardCodedStringLiteral")
+        String background =
+            String.format(Locale.ENGLISH, "action=<%s> type=<%s> detailed state=<%s>",
+                intent.getAction(), intent.getType(), networkInfo.getDetailedState());
+
+        // These if statements and this functionality is heavily inspired by
+        // http://stackoverflow.com/a/5890104/473672
+        if(WifiManager.NETWORK_STATE_CHANGED_ACTION.equals(intent.getAction())) {
+            if(networkInfo.isConnected()) {
+                Timber.i("WifiReceiver speaking status for: %s", background);
+                WifiPresenter.speakStatus(context);
+                return;
+            }
+        } else if(ConnectivityManager.CONNECTIVITY_ACTION.equals(intent.getAction())) {
+            if(networkInfo.getType() == ConnectivityManager.TYPE_WIFI &&
+                networkInfo.getDetailedState() == NetworkInfo.DetailedState.DISCONNECTED)
+            {
+                Timber.i("WifiReceiver speaking status for: %s", background);
+                WifiPresenter.speakStatus(context);
+                return;
+            }
+        } else {
+            Timber.w("WifiReceiver got unexpected action %s", intent.getAction());
         }
 
-        NetworkInfo.State state = networkInfo.getState();
-        if (state != NetworkInfo.State.CONNECTED && state != NetworkInfo.State.DISCONNECTED) {
-            Timber.d("WifiReceiver got non-final network state %s, ignoring", state);
-            return;
-        }
-
-        WifiPresenter.speakStatus(context);
+        Timber.i("WifiReceiver not speaking status for: %s", background);
     }
 }
